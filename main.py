005022fbb63f4674ff1813db19a4db42ac6d49d8
boykjgis <boykjgis@gmail.com>
2023-06-07 13:12:42 -0600

    main.py: Implement dictionary for future requests

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import os
 cwd = os.getcwd()
 url = 'https://ftpgeoinfo.msl.mt.gov/Data/Spatial/MSDI/Cadastral/Parcels/Madison/Madison_GDB.zip'
 
+url_dict = {
+    '1' : 'https://ftpgeoinfo.msl.mt.gov/Data/Spatial/MSDI/Cadastral/Parcels/Madison/Madison_GDB.zip'
+}
+
 def main():
     print('Current Directory: %s' % cwd)
     r = requests.get(url)
