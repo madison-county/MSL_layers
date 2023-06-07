05f85ef04ce454bd584ca8a44c9937e380656d93
boykjgis <boykjgis@gmail.com>
2023-06-07 13:09:53 -0600

    main.py: Fix zip download and import os for cwd download

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import requests
 import zipfile
 import io
+import os
 
+
+cwd = os.getcwd()
 url = 'https://ftpgeoinfo.msl.mt.gov/Data/Spatial/MSDI/Cadastral/Parcels/Madison/Madison_GDB.zip'
 
 def main():
+    print('Current Directory: %s' % cwd)
     r = requests.get(url)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall()
 
 if __name__ == '__main__':
     main()
