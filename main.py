1eb77890e8863538352f69c3f369d69b5107cf5c
boykjgis <boykjgis@gmail.com>
2023-06-07 13:06:51 -0600

    main.py: Add additional imports for zip files and StringIO

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import requests
+import zipfile
+import StringIO
+
+url = 'https://ftpgeoinfo.msl.mt.gov/Data/Spatial/MSDI/Cadastral/Parcels/Madison/Madison_GDB.zip'
 
 def main():
-    pass
+    x = requests.post(url)
 
 if __name__ == '__main__':
     main()
